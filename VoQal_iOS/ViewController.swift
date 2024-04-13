b606d926757bfec87017ddf1c38032c1f21a77ab
SongKyuSeob <sks5075@naver.com>
2024-04-13 22:21:17 +0900

    Initial Commit

---
+++ VoQal_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoQal_iOS
+//
+//  Created by 송규섭 on 2024/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
