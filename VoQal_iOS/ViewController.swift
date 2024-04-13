7c6956670540bdb2c41f714626c14bb18d65b585
SongKyuSeob <sks5075@naver.com>
2024-04-14 00:31:33 +0900

    no storyboard setting

--- VoQal_iOS/ViewController.swift
+++ VoQal_iOS/ViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .secondarySystemBackground
+        
     }
-
 
 }
 
