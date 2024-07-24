cd6540644ff1182e64d72a07bff461171b06219a
SongKyuSeob <sks5075@naver.com>
2024-07-25 00:39:41 +0900

    수업 일지 작성 UI 구현
    위와 동일

---
+++ VoQal_iOS/Components/CustomTextView.swift
@@ -0,0 +1,32 @@
+//
+//  CustomTextView.swift
+//  VoQal_iOS
+//
+//  Created by 송규섭 on 7/24/24.
+//
+
+import UIKit
+
+//class CustomTextView: UITextView {
+//    override init(frame: CGRect, textContainer: NSTextContainer?) {
+//        super.init(frame: frame, textContainer: NSTextContainer?)
+//        
+//        setup()
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        setup()
+//    }
+//    
+//    private func setup() {
+//        // 공통 스타일 설정
+//        self.textColor = .white
+//        self.backgroundColor = UIColor(hexCode: "111111")
+//        self.layer.cornerRadius = 8
+//        self.layer.borderWidth = 1
+//        self.layer.borderColor = UIColor(hexCode: "474747").cgColor
+//        self.font = UIFont(name: "SUIT-Regular", size: 16)
+////        self.leftAnchor =
+//    }
+//}
---
+++ VoQal_iOS/Networking/Lesson/Coach/WriteLessonNoteManager.swift
@@ -0,0 +1,12 @@
+//
+//  WriteLessonNoteManager.swift
+//  VoQal_iOS
+//
+//  Created by 송규섭 on 7/24/24.
+//
+
+import Foundation
+
+struct WriteLessonNoteManager {
+    
+}
