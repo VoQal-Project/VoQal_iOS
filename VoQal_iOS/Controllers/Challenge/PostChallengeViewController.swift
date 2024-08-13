cafaa60c215d65944650d8f6906500938b42643a
SongKyuSeob <sks5075@naver.com>
2024-08-13 22:28:20 +0900

    image Crop 라이브러리 도입, 챌린지 작성 페이지 내 UI 업데이트 및 갤러리 접근 후 크롭 기능 적용
    - CropViewController 도입
    - 이미지 PHPickerController 이용해 가져와 라이브러리를 통해 이미지 크롭 후 원래 뷰에 반영

--- VoQal_iOS/Controllers/Challenge/PostChallengeViewController.swift
+++ VoQal_iOS/Controllers/Challenge/PostChallengeViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import PhotosUI
+import CropViewController
 
-class PostChallengeViewController: BaseViewController {
+class PostChallengeViewController: BaseViewController, PHPickerViewControllerDelegate, CropViewControllerDelegate {
 
     private let postChallengeView = PostChallengeView()
+    private var recordFile: URL? = nil
+    private var thumbnailImage: UIImage? = nil
     
     override func loadView() {
         view = postChallengeView
@@ -21,6 +25,93 @@ class PostChallengeViewController: BaseViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func setAddTarget() {
+        postChallengeView.uploadRecordFileButton.addTarget(self, action: #selector(didTapUploadRecordFileButton), for: .touchUpInside)
+        postChallengeView.uploadThumbnailButton.addTarget(self, action: #selector(didTapUploadThumbnailButton), for: .touchUpInside)
+        postChallengeView.closeButton.addTarget(self, action: #selector(didTapCloseButton), for: .touchUpInside)
+        postChallengeView.postButton.addTarget(self, action: #selector(didTapPostButton), for: .touchUpInside)
+    }
 
+    @objc private func didTapUploadRecordFileButton() {
+        
+    }
+    
+    @objc private func didTapUploadThumbnailButton() {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 1
+        configuration.filter = .images
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        self.present(picker, animated: true)
+        
+    }
+    
+    @objc private func didTapCloseButton() {
+        self.dismiss(animated: true)
+    }
+    
+    @objc private func didTapPostButton() {
+        // .post api request
+        if validateAllFields() {
+            print("post success")
+        }
+        else {
+            print("post failed!")
+        }
+    }
+    
+    private func validateAllFields() -> Bool {
+        var errorFields: [String] = []
+        
+        if postChallengeView.getArtistValue() == "" { errorFields.append("가수명") }
+        if postChallengeView.getSongTitleValue() == "" { errorFields.append("제목(곡 명)") }
+        if self.thumbnailImage == nil { errorFields.append("대표 사진(썸네일)") }
+        if self.recordFile == nil { errorFields.append("음성 파일") }
+        
+        if !errorFields.isEmpty {
+            let alert = UIAlertController(title: "", message: "\(errorFields.joined(separator: ", "))을 입력해주세요!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .default))
+            
+            self.present(alert, animated: false)
+            
+            return false
+        }
+        
+        return true
+    }
 
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        if let result = results.first {
+            result.itemProvider.loadObject(ofClass: UIImage.self) { image, error in
+                if let selectedImage = image as? UIImage {
+                    DispatchQueue.main.async {
+                        self.presentImageCropper(with: selectedImage)
+                    }
+                }
+            }
+        }
+    }
+    
+    func presentImageCropper(with image: UIImage) {
+        let cropViewController = CropViewController(image: image)
+        cropViewController.delegate = self
+        cropViewController.customAspectRatio = CGSize(width: 3, height: 4)
+        cropViewController.aspectRatioLockEnabled = true
+        cropViewController.resetAspectRatioEnabled = false
+        present(cropViewController, animated: true, completion: nil)
+    }
+    
+    func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        thumbnailImage = image
+        self.postChallengeView.setupThumbnailImageView(image)
+        
+        cropViewController.dismiss(animated: true)
+    }
+    
+    func cropViewController(_ cropViewController: CropViewController, didFinishCancelled cancelled: Bool) {
+        cropViewController.dismiss(animated: true)
+    }
+    
 }
--- VoQal_iOS/Views/Challenge/PostChallengeView.swift
+++ VoQal_iOS/Views/Challenge/PostChallengeView.swift
@@ -12,7 +12,8 @@ class PostChallengeView: BaseView {
     private let dimmedView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .black.withAlphaComponent(0.5)
+        view.backgroundColor = .black
+        view.alpha = 0.7
         
         return view
     }()
@@ -20,6 +21,8 @@ class PostChallengeView: BaseView {
     private let contentView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 10.0
+        view.layer.cornerCurve = .continuous
         view.backgroundColor = UIColor(named: "mainBackgroundColor")
         
         return view
@@ -43,6 +46,7 @@ class PostChallengeView: BaseView {
         button.backgroundColor = UIColor(named: "mainButtonColor")
         button.layer.cornerRadius = 10.0
         button.layer.cornerCurve = .continuous
+        button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
     }()
@@ -53,41 +57,61 @@ class PostChallengeView: BaseView {
         textField.layer.cornerCurve = .continuous
         textField.layer.borderColor = UIColor(hexCode: "474747", alpha: 1.0).cgColor
         textField.layer.borderWidth = 2.0
+        textField.isEnabled = false
         textField.translatesAutoresizingMaskIntoConstraints = false
         
         return textField
     }()
     
-    private let uploadRecordFileButton: UIButton = {
+    internal let uploadRecordFileButton: UIButton = {
         let button = UIButton()
-        button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        let configuration = UIImage.SymbolConfiguration(pointSize: 25)
+        let image = UIImage(systemName: "square.and.arrow.up", withConfiguration: configuration)
+        button.setImage(image, for: .normal)
         button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
     }()
     
-    private let uploadThumbnailButton: UIButton = {
+    internal let uploadThumbnailButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.layer.cornerCurve = .continuous
         button.layer.cornerRadius = 10.0
         button.backgroundColor = UIColor(hexCode: "474747", alpha: 1.0)
-        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        
+        let configuration = UIImage.SymbolConfiguration(pointSize: 32)
+        let image = UIImage(systemName: "plus", withConfiguration: configuration)
+        
+        button.setImage(image, for: .normal)
         button.tintColor = .white
         
         return button
     }()
     
-    private let songTitleField: UITextField = {
-        let textField = UITextField()
+    private let ThumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isHidden = true
+        imageView.layer.cornerCurve = .continuous
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10.0
+        
+        return imageView
+    }()
+    
+    private let songTitleField: CustomTextField = {
+        let textField = CustomTextField()
+        textField.placeholder = "제목을 입력해주세요."
         textField.translatesAutoresizingMaskIntoConstraints = false
         
         return textField
     }()
     
-    private let artistField: UITextField = {
-        let textField = UITextField()
+    private let artistField: CustomTextField = {
+        let textField = CustomTextField()
+        textField.placeholder = "가수명을 입력해주세요."
         textField.translatesAutoresizingMaskIntoConstraints = false
         
         return textField
@@ -95,6 +119,7 @@ class PostChallengeView: BaseView {
     
     private let separatorLine: UIView = {
         let view = UIView()
+        view.backgroundColor = UIColor(hexCode: "474747", alpha: 1.0)
         view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
@@ -102,6 +127,7 @@ class PostChallengeView: BaseView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
@@ -109,11 +135,89 @@ class PostChallengeView: BaseView {
     }
     
     override func addSubViews() {
-        
+        addSubview(dimmedView)
+        addSubview(contentView)
+        addSubview(closeButton)
+        contentView.addSubview(recordFileField)
+        contentView.addSubview(uploadRecordFileButton)
+        contentView.addSubview(uploadThumbnailButton)
+        contentView.addSubview(ThumbnailImageView)
+        contentView.addSubview(songTitleField)
+        contentView.addSubview(artistField)
+        contentView.addSubview(separatorLine)
+        contentView.addSubview(postButton)
     }
     
     override func setConstraints() {
-        
+        NSLayoutConstraint.activate([
+            dimmedView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            dimmedView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            dimmedView.topAnchor.constraint(equalTo: topAnchor),
+            dimmedView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            contentView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            contentView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            contentView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 80),
+            contentView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -85),
+            
+            closeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            closeButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            closeButton.widthAnchor.constraint(equalToConstant: 35),
+            closeButton.heightAnchor.constraint(equalToConstant: 35),
+            
+            recordFileField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 25),
+            recordFileField.heightAnchor.constraint(equalToConstant: 40),
+            recordFileField.topAnchor.constraint(equalTo: closeButton.bottomAnchor, constant: 35),
+            recordFileField.trailingAnchor.constraint(equalTo: uploadRecordFileButton.leadingAnchor, constant: -15),
+            
+            uploadRecordFileButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            uploadRecordFileButton.centerYAnchor.constraint(equalTo: recordFileField.centerYAnchor),
+            uploadRecordFileButton.widthAnchor.constraint(equalToConstant: 30),
+            uploadRecordFileButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            uploadThumbnailButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            uploadThumbnailButton.topAnchor.constraint(equalTo: recordFileField.bottomAnchor, constant: 50),
+            uploadThumbnailButton.widthAnchor.constraint(equalToConstant: 115),
+            uploadThumbnailButton.heightAnchor.constraint(equalToConstant: 115),
+            
+            ThumbnailImageView.topAnchor.constraint(equalTo: recordFileField.bottomAnchor, constant: 15),
+            ThumbnailImageView.bottomAnchor.constraint(equalTo: songTitleField.topAnchor, constant: -15),
+            ThumbnailImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            ThumbnailImageView.widthAnchor.constraint(equalTo: ThumbnailImageView.heightAnchor, multiplier: 3.0/4.0),
+            
+            songTitleField.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            songTitleField.topAnchor.constraint(equalTo: uploadThumbnailButton.bottomAnchor, constant: 55),
+            songTitleField.heightAnchor.constraint(equalToConstant: 40),
+            songTitleField.widthAnchor.constraint(equalToConstant: 260),
+            
+            artistField.leadingAnchor.constraint(equalTo: songTitleField.leadingAnchor),
+            artistField.trailingAnchor.constraint(equalTo: songTitleField.trailingAnchor),
+            artistField.heightAnchor.constraint(equalToConstant: 40),
+            artistField.topAnchor.constraint(equalTo: songTitleField.bottomAnchor, constant: 20),
+            
+            separatorLine.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            separatorLine.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            separatorLine.topAnchor.constraint(equalTo: artistField.bottomAnchor, constant: 45),
+            separatorLine.heightAnchor.constraint(equalToConstant: 1),
+            
+            postButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 120),
+            postButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -120),
+            postButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -25),
+            postButton.heightAnchor.constraint(equalToConstant: 35),
+        ])
+    }
+    
+    func getSongTitleValue() -> String? {
+        return songTitleField.text
+    }
+    
+    func getArtistValue() -> String? {
+        return artistField.text
     }
 
+    func setupThumbnailImageView(_ image: UIImage) {
+        self.ThumbnailImageView.image = image
+        self.ThumbnailImageView.isHidden = false
+    }
+    
 }
