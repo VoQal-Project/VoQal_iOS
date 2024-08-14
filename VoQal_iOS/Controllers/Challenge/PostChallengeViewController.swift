70377b280d8aa613e24006a4d34352e77bb6aac6
SongKyuSeob <sks5075@naver.com>
2024-08-15 02:17:13 +0900

    챌린지 작성 페이지 UI 및 기능 구현
    위와 동일
    <보완사항> 녹음 파일 임시 업로드 시 수정 삭제 기능 도입 생각해봐야함.

--- VoQal_iOS/Controllers/Challenge/PostChallengeViewController.swift
+++ VoQal_iOS/Controllers/Challenge/PostChallengeViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 import PhotosUI
 import CropViewController
 
-class PostChallengeViewController: BaseViewController, PHPickerViewControllerDelegate, CropViewControllerDelegate {
+class PostChallengeViewController: BaseViewController, PHPickerViewControllerDelegate, CropViewControllerDelegate, UIDocumentPickerDelegate {
 
     private let postChallengeView = PostChallengeView()
-    private var recordFile: URL? = nil
+    private let postChallengeManager = PostChallengeManager()
+    private var recordFileURL: URL? = nil
     private var thumbnailImage: UIImage? = nil
     
     override func loadView() {
@@ -32,8 +33,16 @@ class PostChallengeViewController: BaseViewController, PHPickerViewControllerDel
         postChallengeView.postButton.addTarget(self, action: #selector(didTapPostButton), for: .touchUpInside)
     }
 
+    
+    
     @objc private func didTapUploadRecordFileButton() {
+        print("uploadRecordFile Tap!")
         
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [UTType.audio])
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false
+        documentPicker.modalPresentationStyle = .overFullScreen
+        present(documentPicker, animated: true, completion: nil)
     }
     
     @objc private func didTapUploadThumbnailButton() {
@@ -52,33 +61,42 @@ class PostChallengeViewController: BaseViewController, PHPickerViewControllerDel
     
     @objc private func didTapPostButton() {
         // .post api request
-        if validateAllFields() {
-            print("post success")
-        }
-        else {
-            print("post failed!")
-        }
-    }
-    
-    private func validateAllFields() -> Bool {
         var errorFields: [String] = []
         
         if postChallengeView.getArtistValue() == "" { errorFields.append("가수명") }
         if postChallengeView.getSongTitleValue() == "" { errorFields.append("제목(곡 명)") }
         if self.thumbnailImage == nil { errorFields.append("대표 사진(썸네일)") }
-        if self.recordFile == nil { errorFields.append("음성 파일") }
+        if self.recordFileURL == nil { errorFields.append("음성 파일") }
         
         if !errorFields.isEmpty {
             let alert = UIAlertController(title: "", message: "\(errorFields.joined(separator: ", "))을 입력해주세요!", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "확인", style: .default))
             
             self.present(alert, animated: false)
-            
-            return false
+            return
         }
         
-        return true
+        postChallengeManager.postChallenge(thumbnail: self.thumbnailImage!, songTitle: postChallengeView.getSongTitleValue()!, singer: postChallengeView.getArtistValue()!, fileURL: self.recordFileURL!) { model in
+            guard let model = model else { print("postChallengeViewController - model 바인딩 실패"); return }
+            
+            if model.status == 200 {
+                let alert = UIAlertController(title: "게시 완료!", message: "챌린지 게시가 완료되었습니다.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
+                    self.dismiss(animated: true)
+                }))
+                
+                self.present(alert, animated: true)
+            }
+            else {
+                let alert = UIAlertController(title: "게시 실패", message: "챌린지 게시에 실패했습니다.\n잠시 후 다시 시도해주세요.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default))
+                
+                self.present(alert, animated: true)
+            }
+        } // 이전에 null인지 검사를 했으나 이를 그대로 쓸 방법을 찾아야함.
     }
+    
+    
 
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         picker.dismiss(animated: true, completion: nil)
@@ -105,13 +123,70 @@ class PostChallengeViewController: BaseViewController, PHPickerViewControllerDel
     
     func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
         thumbnailImage = image
-        self.postChallengeView.setupThumbnailImageView(image)
+        self.postChallengeView.setupThumbnailImageView(image, self, #selector(didTapThumbnailImageView))
         
         cropViewController.dismiss(animated: true)
     }
     
     func cropViewController(_ cropViewController: CropViewController, didFinishCancelled cancelled: Bool) {
         cropViewController.dismiss(animated: true)
+    }
+    
+    @objc private func didTapThumbnailImageView() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "수정", style: .default, handler: { _ in
+            var configuration = PHPickerConfiguration()
+            configuration.selectionLimit = 1
+            configuration.filter = .images
+            let picker = PHPickerViewController(configuration: configuration)
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }))
+        alert.addAction(UIAlertAction(title: "삭제", style: .destructive, handler: { _ in
+            self.postChallengeView.resetThumbnailImageView()
+            self.thumbnailImage = nil
+        }))
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let selectedFileURL = urls.first else { return }
+        
+        // 선택된 파일 경로 확인
+        print("Selected File URL: \(selectedFileURL)")
+        
+        // 보안 범위 시작
+        guard selectedFileURL.startAccessingSecurityScopedResource() else {
+            print("파일에 접근할 수 없습니다.")
+            return
+        }
+        
+        defer {
+            // 보안 범위 종료
+            selectedFileURL.stopAccessingSecurityScopedResource()
+        }
+        
+        // 파일을 앱의 Documents 디렉토리로 복사
+        let fileManager = FileManager.default
+        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let destinationURL = documentsDirectory.appendingPathComponent(selectedFileURL.lastPathComponent)
+        
+        do {
+            // 기존 파일이 있다면 삭제
+            if fileManager.fileExists(atPath: destinationURL.path) {
+                try fileManager.removeItem(at: destinationURL)
+            }
+            // 파일 복사
+            try fileManager.copyItem(at: selectedFileURL, to: destinationURL)
+            self.recordFileURL = destinationURL
+            
+            self.postChallengeView.setupRecordFileField(destinationURL)
+            
+        } catch {
+            print("파일 복사 중 오류 발생: \(error)")
+        }
     }
     
 }
